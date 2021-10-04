8f1c6b56023af573c5d22612ed63262a2c586ad8
Tolibjon <tolibjohn@gmail.com>
2021-10-04 17:41:14 +0500

    Add getters to the class `Person`

--- main.rb
+++ main.rb
@@ -1,11 +1,21 @@
 class Person
-  initialize(name="Unknown", age, parent_permission=true)
+  def initialize(name="Unknown", age, parent_permission:true)
     @id=id
     @name=name
     @age=age
+    @parent_permission=parent_permission
   end
 
+  def id
+    @id
+  end
   
+  def name
+    @name
+  end
+
+  def age
+    @age
+  end
 
 end
-
